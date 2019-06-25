ceca3c36849e79a425df0e0c94295054cc6b7d68
LuizASSilveira <luiz_ssilveira@hotmail.com>
2019-06-24 21:47:07 -0300

    aplicando psd_welch

--- psb/projeto2/projeto2.py
+++ psb/projeto2/projeto2.py
@@ -2,14 +2,16 @@ import pandas as pd
 import mne
 from mne.time_frequency import psd_welch as pw
 
-#bandpass mais vezes(fmin, fmax)
-#tmin, tmax deslizamento de janela
+# bandpass mais vezes(fmin, fmax)
+# tmin, tmax deslizamento de janela
 
-#usar
-#from.mne.decoding import PSDEstimator
-# psd = PSDEstimator(sfreq,fmin,fmax)
+# usar
+# from.mne.decoding import PSDEstimator
+# psd = PSDEstimator(sfreq,fmin = 0,fmax = 128)
 # x= psd.transform(raw) 
-#FilterEstimator
+# FilterEstimator
+
+#usar filtro passaFaixa
 
 def testData(df):
     lista = []
@@ -35,15 +37,19 @@ def openData(name):
     return df.transpose()
 
 df = openData("entrada.csv")
-
 ch_types = ['eeg'] * 6
 ch_names = ['PO3', 'PO4', 'P8', 'O1', 'O2', 'P7']
-
 
 info = mne.create_info(ch_names= ch_names, sfreq=256 , ch_types=ch_types)
 raw = mne.io.RawArray(df,info)
 montage = mne.channels.read_montage('standard_1020')
 raw.set_montage(montage)
 
-raw.plot_psd()
+raw.filter(5,50)
+raw.filter(5,50)
+raw.filter(5,50)
 
+#raw.plot_psd()
+psds, freqs = pw(raw,fmin=0,fmax=128,tmin=0,tmax=3)#arrumar aki buffer
+print(psds[0][8:12])#0 ah 5
+#usar psd_welch
